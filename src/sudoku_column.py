0982681c58d9bf0a00cd28b8cdab9b3ebe731dfa
Samf33 <144719150+Samf33@users.noreply.github.com>
2025-09-01 17:03:24 -0400

    done

--- src/sudoku_column.py
+++ src/sudoku_column.py
@@ -1,6 +1,25 @@
 # Write your solution here
 def column_correct(list : list, column_num : int):
-    pass
+    column = []
+    count1 = 0
+    for row in list:
+        count1 = 0
+        for value in row:
+            if count1 == column_num:
+                column.append(value)
+            count1 +=1
+    count1 = 0
+    count2 = 0
+    for num in column:
+        count1 += 1
+        for nom in column:
+            count2+=1
+            if nom == num and nom != 0 and num != 0 and count1 != count2:
+                return False
+        count2 = 0
+    return True
+
+    
 
 if __name__ == "__main__":
 
